bf01aa25fb2ad7124271dbb12e61980c9fdeed0e
vasya-ryba <kano.vas.2.0@gmail.com>
2023-08-10 03:01:35 +0400

    variables description

--- variables.tf
+++ variables.tf
@@ -1,12 +1,20 @@
-# DigitalOcean token
-variable "do_token" {}
+variable "do_token" {
+  description = "DigitalOcean token"
+  type = string
+}
 
-# Path to SSH private key
-variable "pvt_key" {}
+variable "pvt_key" {
+  description = "Path to SSH private key"
+  type = string
+}
 
-# Name of private key at DigitalOcean
-variable "pvt_key_name" {}
+variable "pvt_key_name" {
+  description = "Name of private key at DigitalOcean"
+  type = string
+}
 
-# IP address(-es) of host which will be used to connect to the cluster
-# Example: 4.3.2.1/24
-variable jumpbox_ip {}
+variable jumpbox_ip {
+  description = "IP address(-es) of host which will be used to connect to the cluster. Example: 4.3.2.1/24"
+  type = string
+  default = "127.0.0.1"
+}
